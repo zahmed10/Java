b266788580278be6b90e71413ed8abca04a669d1
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-06-12 23:06:08 -0700

    fix mispelling in FirstJavaProgram.java

--- JavaFoundations/Fundamentals/FirstJavaProgram/FirstJavaProgram.java
+++ JavaFoundations/Fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -2,7 +2,7 @@ public class FirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("My name is Zayd Ahmed");
         System.out.println("I am 29 years old");
-        System.out.println("MMy hometown is Phoenix, AZ");
+        System.out.println("My hometown is Phoenix, AZ");
 
     }
 }
